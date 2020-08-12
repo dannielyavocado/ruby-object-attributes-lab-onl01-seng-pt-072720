f05cb30988cd768460afd1a2da4f08dff5a00b42
Danniely Santos <dannielyingrid@gmail.com>
2020-08-12 06:53:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 class Person
- 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
-  end
- 
-end
+  def name=(persons_name)
+  @name = persons_name
+ end
+
+def name 
+  @name 
+ end
+
+def job=(persons_job)
+  @job = persons_job
+ end
+
+def job  
+  @job
+ end
+
+end 
